4430ee421f20fdba5fdc3c49ed0021875527938c
Devkub8 <127433472+Devkub8@users.noreply.github.com>
2023-03-10 15:58:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   secret_key = "VH1cX97ZNA7vomewibEE5s9MuRdIUPRfuBRZSU+7"
   region     = "ap-south-1"
 }
-resource "aws_instance" "uday" {
+resource "aws_instance" "jen2" {
     ami = "ami-0d81306eddc614a45"
     instance_type = "t2.micro"
 }
